1ffd891c547b0d8f8df7991efef8b4b4931cd069
tamaki05 <106146818+tamaki05@users.noreply.github.com>
2024-02-02 08:49:36 +0900

    マイグレーション実行

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('first_name_kana')->nullable();
+            $table->string('last_name_kana')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('tel_number');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
