623c3ce55fa106385e0b38c9320b29881ba37471
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2024-02-02 21:58:38 +0900

    migrate-fixes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('first_name_kana')->nullable();
-            $table->string('last_name_kana')->nullable();
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('tel_number');
